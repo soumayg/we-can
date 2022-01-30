d95147be4c7c246a88826ab53f53ef43542c58a9
soumayg9673 <pmrs9673@gmail.com>
2022-01-31 01:51:27 +0530

    constant file changes for security

--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -1,8 +1,8 @@
 //MapMyIndiaCredentials
-const String kMapMyIndiaRestApiKey = '08b661dc48cb4d20b88447e7a87da393';
+const String kMapMyIndiaRestApiKey = 'R08b661dc48cb4d20b88447e7a87da393A';
 const String kGrantType = 'grant_type=client_credentials';
-const String kClientId = '33OkryzDZsIA7O6O8QLNGjxDHj23G4BkkOSqTA8SxMPJav2mYq4KU3qLJgqSQGGrmW04Bk-n32O5XMu7MwV4aNkNDfv7ONjt';
-const String kClientSecret = 'lrFxI-iSEg_YwANSDV-SkZh7IIatkTmBn39SI893GDBMaLujuf5ihaqpeZp0zd0UOKxfo3Z6byxH43AX_iR_qbjHyz--AW0U7trA3yVupZU=';
+const String kClientId = 'R33OkryzDZsIA7O6O8QLNGjxDHj23G4BkkOSqTA8SxMPJav2mYq4KU3qLJgqSQGGrmW04Bk-n32O5XMu7MwV4aNkNDfv7ONjtA';
+const String kClientSecret = 'RlrFxI-iSEg_YwANSDV-SkZh7IIatkTmBn39SI893GDBMaLujuf5ihaqpeZp0zd0UOKxfo3Z6byxH43AX_iR_qbjHyz--AW0U7trA3yVupZU=A';
 
 //LocationServiceData
 late String kLocationName = '';
